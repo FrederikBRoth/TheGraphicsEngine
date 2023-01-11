2266849415d173d688dc4f0ba8c2e584caa9279c
Frederik Bøttger-Roth <frederikbroth@gmail.com>
2023-01-11 23:34:47 +0100

    Prepping world class for simple chunk gen

--- headers/world/World.h
+++ headers/world/World.h
@@ -2,12 +2,18 @@
 #define WORLD_H
 #include <vector>
 #include <chunk/Chunk.h>
-
+#include <glm/detail/type_vec3.hpp>
+#include <math.h>
 constexpr int CHUNKSIZE_X = 16, CHUNKSIZE_Y = 16, CHUNKSIZE_Z = 16, CHUNKAREA = CHUNKSIZE_X * CHUNKSIZE_Y,
 CHUNKVOLUME = CHUNKAREA * CHUNKSIZE_Z, WATER_LEVEL = 64;
 class World
 {
-
-	//std::vector<Chunk*> chunks;
+	Chunk* chunks;
+	int worldX, worldY, worldZ;
+	void updateWorldPosition(float x, float y, float z);
+	glm::u32vec3 getWorldPosition();
+	void generateChunk();
+	void update();
+	
 };
 #endif // !WORLD_H
--- src/world/World.cpp
+++ src/world/World.cpp
@@ -1,1 +1,21 @@
 #include <world/World.h>
+
+void World::updateWorldPosition(float x, float y, float z)
+{
+	worldX = (int)floorf(x);
+	worldY = (int)floorf(y);
+	worldZ = (int)floorf(z);
+}
+
+glm::u32vec3 World::getWorldPosition()
+{
+	return glm::u32vec3();
+}
+
+void World::generateChunk()
+{
+}
+
+void World::update()
+{
+}
