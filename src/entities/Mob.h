34b8094e818929de3b4fe5495b351226448f81c3
Frederik Bøttger-Roth <frederikbroth@gmail.com>
2023-03-09 20:03:05 +0100

    I have a panic attack lmaoooo

--- src/entities/Mob.h
+++ src/entities/Mob.h
@@ -8,7 +8,6 @@ class Mob : public Entity
 	std::string name;
 	Mob(glm::vec3 collisionBox, glm::vec3 position, std::string name, float yaw, glm::vec3 scale);
 
-
 	// Inherited via Entity
 	virtual void update() override;
 };
