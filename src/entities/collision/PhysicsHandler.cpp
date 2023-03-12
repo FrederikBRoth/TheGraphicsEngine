3beae8a87a1475d949546636bef4acb2e7a47ca4
Frederik Bøttger-Roth <frederikbroth@gmail.com>
2023-03-12 20:19:56 +0100

    Hej

--- src/entities/collision/PhysicsHandler.cpp
+++ src/entities/collision/PhysicsHandler.cpp
@@ -3,7 +3,6 @@
 void PhysicsHandler::update(std::vector<Entity*> entities, ChunkController* cc, Window* window)
 {
 	for (auto& entity : entities) {
-
 		entity->checkCollision(cc);
 		entity->update();
 		if (entity->velocity.y > -gravity) {
